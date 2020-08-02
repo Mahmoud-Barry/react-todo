63995aab782a851539604bf96278f1eced3d2dd7
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2020-08-02 13:39:34 +0000

    Updates

---
+++ precache-manifest.4f37ef6a032ac8f9812c00ee34caa6e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e787e279f8cbf13a300d583fd28115b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "774a569eee04fb767108",
+    "url": "/react-todo/static/css/main.f637ce00.chunk.css"
+  },
+  {
+    "revision": "eebd2ca626611044ec86",
+    "url": "/react-todo/static/js/2.da9cffb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.da9cffb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774a569eee04fb767108",
+    "url": "/react-todo/static/js/main.3abeacf2.chunk.js"
+  },
+  {
+    "revision": "c1536632fea85d80fba8",
+    "url": "/react-todo/static/js/runtime-main.09e4660a.js"
+  }
+]);
